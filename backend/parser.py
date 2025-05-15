04b628b03d95cd6671772bef93b2d0dfb5b2170b
ako28 <ako28@cornell.edu>
2025-05-15 10:42:08 -0400

    still playing around w/ parser

--- backend/parser.py
+++ backend/parser.py
@@ -5,9 +5,13 @@ class PdfParser:
     def __init__(self):
         pass
 
-    def parseWithMetadata(self, pdf_path):
-        # output = pymupdf4llm.to_markdown(pdf_path, page_chunks=True)
-        output = pymupdf4llm.to_markdown(pdf_path, write_images=True)
+    def simple_parse(self, pdf_path):
+        output = pymupdf4llm.to_markdown(pdf_path)
+        return output
+
+    def parse_page_chunking(self, pdf_path):
+        output = pymupdf4llm.to_markdown(pdf_path, page_chunks=True)
+        # output = pymupdf4llm.to_markdown(pdf_path, write_images=True)
         # output = pymupdf4llm.to_markdown(pdf_path, page_chunks=True, write_images=True)
 
         # with open('output.json', 'w', encoding='utf-8') as f:
@@ -15,6 +19,16 @@ class PdfParser:
 
         # with open('output.txt', 'w', encoding='utf-8') as f:
         #     f.write(output)
+        return output
 
-# parser = PdfParser()
-# parser.parseWithMetadata("./pdftest/fpreport.pdf")
+    def parse_embed_images(self, pdf_path):
+        output = pymupdf4llm.to_markdown(pdf_path, embed_images=True)
+        return output
+
+    def parse_save_images(self, pdf_path, img_path=""):
+        output = pymupdf4llm.to_markdown(pdf_path, write_images=True, image_path=img_path)
+        return output
+
+parser = PdfParser()
+output = parser.parse_embed_images("./test.pdf")
+print(output)
